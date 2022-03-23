9d289d86924035149bb33c54151e907e7cf9132d
jou122 <yousafmohamed30@yahoo.com>
2022-03-23 19:02:52 +0200

    breakpoints
    added validation of input
    added breakpoints

--- SignalsProject.m
+++ SignalsProject.m
@@ -1,14 +1,31 @@
 clc
-disp("Welcome to our function generator /n ")
+disp("Welcome to our function generator ")
 disp("get in with your right leg")
 
 freq=input("Enter the Sampling Frequency of the Signal ");
 start_t=input("Enter the Start time of the Signal ");
 end_t=input("Enter the end time of the Signal ");
 
+while end_t<start_t
+    disp('invalid end time cannot be less than start time ')
+    end_t=input("Enter the end time of the Signal ");
+end 
+
 t = linspace(start_t,end_t,freq);
 
-brk_pts=input("Enter the number of break points in the signal");
+brk_pts_no=input("Enter the number of break points in the signal ");
+brk_pts=ones(1,brk_pts_no);
+i=1;
+while i<=brk_pts_no
+    brk_pts_in=input(sprintf("Enter the time of break point no %d ",i )); 
+    
+    if (brk_pts_in<=start_t || brk_pts_in>=end_t)
+        disp("Input is not in range of the time limit");
+    else
+     brk_pts(i)= brk_pts_in;
+     i=i+1;
+    end 
+end
 
 
 %sherif
@@ -72,6 +89,7 @@ while ( strcmp(txt,"Y") || strcmp(txt,"y"))
             figure
             plot(t,x)
         otherwise
+            break;
            
     end
     txt = input("Do you want to perform any other operation ? Y/N : ","s");
